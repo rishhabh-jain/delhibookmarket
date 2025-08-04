edbff7b6851aff9565e53c8fc80ccf58141bf1c5
SanchitJain28 <mythichuman28@gmail.com>
2025-08-04 19:14:11 +0530

    change

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,17 +2,16 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const { origin, search } = request.nextUrl;
-  const decodedPath = decodeURIComponent(request.nextUrl.pathname);
+  const url = request.nextUrl;
 
-  if (decodedPath === "/shop-2") {
-    const redirectUrl = new URL(`/${search}`, origin);
-    return NextResponse.redirect("/");
+  if (url.pathname === "/shop-2") {
+    url.pathname = "/";
+    return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/shop-2"], // Match everything so you can decode and check
+  matcher: ["/shop-2"],
 };
