2aa845e954b69483f017bdb09288906701435953
SanchitJain28 <mythichuman28@gmail.com>
2025-08-04 13:36:47 +0530

    chsanges

---
+++ scripts/generate-sitemap.ts
@@ -0,0 +1,35 @@
+// scripts/generate-sitemap.ts
+import fs from "fs";
+
+async function fetchAllProducts() {
+  const res = await fetch("https://delhibookmarket.com/api/get-all-products");
+  const products = await res.json();
+
+  return products;
+}
+
+async function generateSitemap() {
+  const siteUrl = "https://delhibookmarket.com";
+  const products = await fetchAllProducts();
+
+  const urls = products
+    .map(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (p: { permalink: string }) => `
+    <url>
+      <loc>${siteUrl}/${p.permalink.split("/").filter(Boolean).pop()}</loc>
+      <lastmod>${new Date()}</lastmod>
+    </url>`
+    )
+    .join("");
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls}
+  </urlset>`;
+
+  fs.writeFileSync("public/sitemap.xml", xml);
+  console.log("✅ sitemap.xml generated!");
+}
+
+generateSitemap();
