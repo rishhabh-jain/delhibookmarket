18a27c6247f36dd07534064aa853f7669212ba6a
SanchitJain28 <mythichuman28@gmail.com>
2025-08-12 18:02:36 +0530

    c

--- src/app/(app)/product/[id]/productClient.tsx
+++ src/app/(app)/product/[id]/productClient.tsx
@@ -331,7 +331,7 @@ export default function ProductClient({ product }: ProductClientProps) {
         </nav>
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mb-6">
-          {/* Product Image  ccccc*/}
+          {/* Product Image  cccccccc*/}
           <div className="lg:col-span-1">
             <div className="sticky top-4">
               <Image
